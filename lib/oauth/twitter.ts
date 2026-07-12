ac8f5cc829bcc358b35e5bd81f5f4f2ce2c73704
MAYSN <j.mejjati9@gmail.com>
2026-07-12 02:56:49 +0100

    Update twitter.ts

--- lib/oauth/twitter.ts
+++ lib/oauth/twitter.ts
@@ -1,7 +1,7 @@
 import { supabase } from "@/lib/supabase";
 
 export async function connectTwitter() {
-  const { error } = await supabase.auth.linkIdentity({
+  const { error } = await supabase.auth.signInWithOAuth({
     provider: "twitter",
     options: {
       redirectTo: `${window.location.origin}/settings/callback?provider=twitter`,
